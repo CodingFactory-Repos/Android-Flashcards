ed784e9be235e3e8389af51a3c31d64829b36a76
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2023-02-21 15:01:25 +0100

    feat(copyright): Ajouts des copyrights dans tous les fichiers
    Ajout des informations de droits d'auteur dans tous les fichiers pour se conformer aux exigences légales et protéger les droits de propriété intellectuelle de l'entreprise.

--- app/src/main/java/me/loule/vroomcards/MainActivity.java
+++ app/src/main/java/me/loule/vroomcards/MainActivity.java
@@ -1,3 +1,11 @@
+/*
+ * MainActivity.java - MainActivity
+ *
+ * Created on 21/02/2023 14:58:33 by loule
+ *
+ * Copyright (c) 2023. loule (https://loule.me) & CodingFactory (https://codingfactory.fr) @ All rights reserved.
+ */
+
 package me.loule.vroomcards;
 
 import androidx.appcompat.app.AppCompatActivity;
