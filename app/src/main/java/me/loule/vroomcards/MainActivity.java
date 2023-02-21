899da3d40dbfc56eaea10305db43cab711a69ec8
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2023-02-21 14:46:23 +0100

    Initialize Project

---
+++ app/src/main/java/me/loule/vroomcards/MainActivity.java
@@ -0,0 +1,13 @@
+package me.loule.vroomcards;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
