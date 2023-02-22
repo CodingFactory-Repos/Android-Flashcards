e9a340491a340ca2b3c48544c51e01edf70bea77
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2023-02-22 10:34:35 +0100

    pushing the branch and pushing the first part of the game

--- app/src/main/java/me/loule/vroomcards/activities/GameActivity.java
+++ app/src/main/java/me/loule/vroomcards/activities/GameActivity.java
@@ -8,9 +8,19 @@
 
 package me.loule.vroomcards.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
 import me.loule.vroomcards.R;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -18,5 +28,28 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        loadQuestionsFromApi();
+
+    }
+    private void loadQuestionsFromApi() {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://flint-tar-shovel.glitch.me/cars/1")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.e("APPEL API", "onFailure", e);
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                assert response.body() != null;
+                String body = response.body().string();
+                Log.i("Response", "onResponse: " + body);
+            }
+        });
     }
 }
