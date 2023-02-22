48d75d6aef2fc98126320f505e368d08eff9886f
Vahé Krikorian <vahe.krikorian@edu.esiee-it.fr>
2023-02-22 10:32:50 +0100

    Home page completly added without style

--- app/src/main/java/me/loule/vroomcards/activities/MainActivity.java
+++ app/src/main/java/me/loule/vroomcards/activities/MainActivity.java
@@ -9,7 +9,16 @@
 package me.loule.vroomcards.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import me.loule.vroomcards.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +27,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadioGroup difficultyRadioGroup = findViewById(R.id.difficultyRadioGroup);
+        ImageView difficultyCloseImageView = findViewById(R.id.difficultyCloseImageView);
+        difficultyCloseImageView.setVisibility(View.INVISIBLE);
+        difficultyRadioGroup.setVisibility(View.INVISIBLE);
+        Button difficultyButton = findViewById(R.id.difficultyButton);
+        Button quizButton = findViewById(R.id.quizButton);
+        Button questionsButton = findViewById(R.id.questionsButton);
+        Button aboutButton = findViewById(R.id.aboutButton);
+
+        difficultyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                difficultyRadioGroup.setVisibility(View.VISIBLE);
+                difficultyCloseImageView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        difficultyRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = findViewById(checkedId);
+                Toast.makeText(MainActivity.this, "La difficulté choisie est : " + radioButton.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        difficultyCloseImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                difficultyRadioGroup.setVisibility(View.INVISIBLE);
+                difficultyCloseImageView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        quizButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        questionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, QuestionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
