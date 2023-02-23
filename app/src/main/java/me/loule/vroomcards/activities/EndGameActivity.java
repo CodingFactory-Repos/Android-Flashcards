c9e8ccd0f62cddfd1ec3d9ea8e738e1dc18b456c
Vahé Krikorian <vahe.krikorian@edu.esiee-it.fr>
2023-02-23 09:42:27 +0100

    Add end Game activity frontEnd

--- app/src/main/java/me/loule/vroomcards/activities/EndGameActivity.java
+++ app/src/main/java/me/loule/vroomcards/activities/EndGameActivity.java
@@ -9,7 +9,12 @@
 package me.loule.vroomcards.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import me.loule.vroomcards.R;
 
 public class EndGameActivity extends AppCompatActivity {
@@ -17,6 +22,17 @@ public class EndGameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_end_game); 
+        setContentView(R.layout.activity_end_game);
+
+        Button returnHomeButton = findViewById(R.id.returnHomeButton);
+
+        returnHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EndGameActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
